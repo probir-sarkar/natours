1f88441a2da6e7ecfa633b355b3a19c19b248e21
Probir Sarkar <Probirsarkar855@gmail.com>
2022-11-15 18:16:01 +0530

    cpanel

--- app.js
+++ app.js
@@ -4,10 +4,6 @@ const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
-// MIDDLEWARES
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
-}
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
