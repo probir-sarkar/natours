53ce9b7fce6bc82ee9cf0fe9110af093cbe604d6
Probir Sarkar <Probirsarkar855@gmail.com>
2022-11-16 07:02:03 +0530

    server

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 // All required modules
 const mongoose = require("mongoose");
-// const dotenv = require("dotenv");
+const dotenv = require("dotenv");
 const app = require("./app");
 // End of required modules
 
 // set environment variables from .env file
-// dotenv.config({ path: "./config.env" });
-// dotenv.config({ path: "./etc/secrets/config.env" });
-
+if (process.env.PORT === undefined) {
+  dotenv.config({ path: "./config.env" });
+}
 // get the database connection string from the environment variables
 const DB = process.env.DATABASE.replace(
   "<PASSWORD>",
@@ -24,7 +24,11 @@ mongoose
   })
   .then(() => console.log("DB connection successful"));
 
-// server will listen on port 3001 for incoming requests
-const port = process.env.PORT || 3001;
+// server will listen on port 3000 for incoming requests
+const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+//  start the server
+
+app.listen(port, () => {
+  console.log(`"Server started on port 3000" ${port}`);
+});
