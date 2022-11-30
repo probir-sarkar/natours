9331f8acdcde2c71d06d1388358f9f82f342811e
probir-sarkar <probirsarkar855@gmail.com>
2022-11-30 08:01:35 +0530

    .

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -24,37 +24,45 @@ const Tour = require("../models/tourModels");
 // };
 
 exports.getAllTours = async (req, res) => {
-  const queryObj = { ...req.query };
-  const excludedFields = ["page", "sort", "limit", "fields"];
-  excludedFields.forEach((el) => delete queryObj[el]);
-
-  // 2
-  let queryStr = JSON.stringify(queryObj);
-  queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-  queryStr = JSON.parse(queryStr);
-
-  let query = Tour.find(queryStr);
-  // 3 sort
-  if (req.query.sort) {
-    const sortBy = req.query.sort.replace(/,/g, " ");
-    query = query.sort(sortBy);
-  } else {
-    query = query.sort("-createdAt");
-  }
-  // 4 field limiting
-  if (req.query.fields) {
-    const fields = req.query.fields.replace(/,/g, " ");
-    query = query.select(fields);
-  } else {
-    query = query.select("-__v");
-  }
-  // 5 pagination
-  const page = +req.query.page || 1;
-  const limit = +req.query.limit || 100;
-  const skip = (page - 1) * limit;
-  query = query.skip(skip).limit(limit);
-
   try {
+    // BUILD QUERY
+    const queryObj = { ...req.query };
+
+    // 1) Filtering
+    // excluding fields from queryObj that are not in the model schema
+    const excludedFields = ["page", "sort", "limit", "fields"];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    // 2) Advanced filtering
+    // converting queryObj to string and replacing the operators with $ operators
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    queryStr = JSON.parse(queryStr);
+    let query = Tour.find(queryStr);
+    // 3 sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.replace(/,/g, " ");
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort("-createdAt");
+    }
+
+    // 4 field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.replace(/,/g, " ");
+      query = query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
+    // 5 pagination
+    const page = +req.query.page || 1;
+    const limit = +req.query.limit || 100;
+    const skip = (page - 1) * limit;
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip > numTours) throw new Error("This page does not exist");
+    }
+    query = query.skip(skip).limit(limit);
     const tours = await Tour.find(query);
     res.status(200).json({
       status: "success",
